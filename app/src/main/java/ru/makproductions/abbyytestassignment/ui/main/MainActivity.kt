3dd6a3feb77fae2527560b52aba42b86d0c6c50a
ro-mak <eee127@yandex.ru>
2019-05-28 18:34:57 +0300

    Initial commit

---
+++ app/src/main/java/ru/makproductions/abbyytestassignment/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.makproductions.abbyytestassignment.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ru.makproductions.abbyytestassignment.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
